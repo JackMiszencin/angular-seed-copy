a201317a2710ed38ff57c0c06d733c84edf77d32
Jack Miszencin <jack.miszencin@gmail.com>
2014-04-08 17:27:43 -0400

    Got original tests working, need to refactor and get rid of unnecessary scope definitions

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,20 +1,41 @@
 'use strict';
 
 /* jasmine specs for controllers go here */
+describe('animals animalsControllers', function(){
+  beforeEach(module('animals'));
+  beforeEach(module('animalsServices'));
 
-describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+  describe('controllers', function(){
+    var scope, ctrl, $httpBackend;
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller){
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('animals/animals.json').respond([{name:"Dogs"},{name:"Pigs (Three Diffent Ones)"}]);
+      scope = $rootScope.$new();
+      ctrl = $controller('MyCtrl1', {$scope: scope});
+    }));
 
 
-  it('should ....', inject(function($controller) {
-    //spec body
-    var myCtrl1 = $controller('MyCtrl1');
-    expect(myCtrl1).toBeDefined();
-  }));
+    it('should ....', inject(function($controller) {
+      //spec body
+      var myCtrl1 = $controller('MyCtrl1');
+      expect(myCtrl1).toBeDefined();
+    }));
+  });
 
-  it('should ....', inject(function($controller) {
-    //spec body
-    var myCtrl2 = $controller('MyCtrl2');
-    expect(myCtrl2).toBeDefined();
-  }));
+  describe('controllers', function(){
+    var scope, ctrl, $httpBackend;
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller){
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('animals/animals.json').respond([{name:"Dogs"},{name:"Pigs (Three Diffent Ones)"}]);
+      scope = $rootScope.$new();
+      ctrl = $controller('MyCtrl2', {$scope: scope});
+    }));
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var myCtrl2 = $controller('MyCtrl2');
+      expect(myCtrl2).toBeDefined();
+    }));
+  });
+
 });
