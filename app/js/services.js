7b0163641ab1d5079ef69420c312e250e178358f
Jack Miszencin <jack.miszencin@gmail.com>
2014-04-08 16:22:35 -0400

    Version error finally fixed. Hallelujah jesusfish amen.

--- app/js/services.js
+++ app/js/services.js
@@ -7,6 +7,8 @@
 // In this case it is a simple value service.
 var animalsServices = angular.module('animalsServices', ['ngResource']);
 
+animalsServices.value('version', '0.1');
+
 animalsServices.factory('Animal', ['$resource', function($resource){
 	return $resource('animals/:animalId.json', {}, {
 		query: {method:'GET', params:{animalId:'animals'}, isArray:true}
