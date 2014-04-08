760b5044cb6244927afd921944b9032583ea5acf
Jack Miszencin <jack.miszencin@gmail.com>
2014-04-08 17:39:09 -0400

    Got rid of superfluous code added into controllers spec

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -6,36 +6,23 @@ describe('animals animalsControllers', function(){
   beforeEach(module('animalsServices'));
 
   describe('controllers', function(){
-    var scope, ctrl, $httpBackend;
-    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller){
-      $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('animals/animals.json').respond([{name:"Dogs"},{name:"Pigs (Three Diffent Ones)"}]);
-      scope = $rootScope.$new();
-      ctrl = $controller('MyCtrl1', {$scope: scope});
-    }));
-
 
     it('should ....', inject(function($controller) {
       //spec body
       var myCtrl1 = $controller('MyCtrl1');
       expect(myCtrl1).toBeDefined();
     }));
+
   });
 
   describe('controllers', function(){
-    var scope, ctrl, $httpBackend;
-    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller){
-      $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('animals/animals.json').respond([{name:"Dogs"},{name:"Pigs (Three Diffent Ones)"}]);
-      scope = $rootScope.$new();
-      ctrl = $controller('MyCtrl2', {$scope: scope});
-    }));
 
     it('should ....', inject(function($controller) {
       //spec body
       var myCtrl2 = $controller('MyCtrl2');
       expect(myCtrl2).toBeDefined();
     }));
+
   });
 
 });
