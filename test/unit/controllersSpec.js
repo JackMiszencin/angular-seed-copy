fb284c7942c560e3773e9f362f955d5311266a1b
Jack Miszencin <jack.miszencin@gmail.com>
2014-04-08 17:58:27 -0400

    Got new AnimalsCtrl test working

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -5,6 +5,16 @@ describe('animals animalsControllers', function(){
   beforeEach(module('animals'));
   beforeEach(module('animalsServices'));
 
+  beforeEach(function(){
+    this.addMatchers({
+      toEqualData: function(expected){
+        return angular.equals(this.actual, expected);
+      }
+    })
+  });
+
+  var animalsResponse = [{name:"Dogs"},{name:"Pigs (Three Different Ones)"}];
+
   describe('controllers', function(){
 
     it('should ....', inject(function($controller) {
@@ -23,6 +33,16 @@ describe('animals animalsControllers', function(){
       expect(myCtrl2).toBeDefined();
     }));
 
+  });
+
+  describe('AnimalsController', function(){
+    it('should give back both pigs and dogs', inject(function($controller, _$httpBackend_, $rootScope){
+      _$httpBackend_.expectGET('animals/animals.json').respond([{name:"Dogs"},{name:"Pigs (Three Different Ones)"}]);
+      var scope = $rootScope.$new();
+      var aniCtrl = $controller('AnimalsCtrl', {$scope: scope})
+      _$httpBackend_.flush();
+      expect(scope.animals).toEqualData(animalsResponse);
+    }));
   });
 
 });
